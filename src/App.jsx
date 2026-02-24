ae2a7ad767e523059583c290a338308dbb14dcab
kmTyagi198 <k.m.tyagi198@gmail.com>
2026-02-25 00:39:29 +0530

    created context file

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-        <ClickCounterer />
+        <ClickCounter />
     </>
   )
 }
---
+++ src/components/ClickContext.js
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+export const ClickCounterContext = createContext();
+
+export default function ClickContext({children}) {
+
+    const [clickCounter, setClickCounter] = useState(0);
+
+
+    return (
+    <>
+        <ClickCounterContext.Provider 
+            value={{ clickCounter, setClickCounter }}>
+        {children}
+        </ClickCounterContext.Provider>
+    </>
+    )
+}
